3458d805811c4942a67252b9d82b005a8c431a56
Pdom <ahb0327@naver.com>
2020-10-14 14:20:11 +0900

    Update: 공통 코드를 위한 설정 변경

--- libs/m/src/server/index.ts
+++ libs/m/src/server/index.ts
@@ -5,7 +5,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 app.listen(port, function () {
   console.log(`http://localhost:${port}`);
