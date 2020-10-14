331b5696dfdf3a5a65dc9a8f7d6c7c41966b4b7e
Pdom <ahb0327@naver.com>
2020-10-14 14:21:29 +0900

    Add: socket.io

--- libs/m/src/client/index.ts
+++ libs/m/src/client/index.ts
@@ -1,4 +1,14 @@
 import * as PIXI from 'pixi.js';
+import io from 'socket.io-client';
+
+const socket = io({
+  reconnectionDelayMax: 10000,
+  query: {
+    auth: '123',
+  },
+});
+
+socket.emit('chat message', '메세지 입니다.');
 
 const app = new PIXI.Application({ backgroundColor: 0x1099bb });
 app.renderer.autoDensity = true;
--- libs/m/src/server/index.ts
+++ libs/m/src/server/index.ts
@@ -1,13 +1,28 @@
 import express from 'express';
+import http from 'http';
+import socketIO from 'socket.io';
 import path from 'path';
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 
-app.listen(port, function () {
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    console.log(msg);
+  });
+});
+
+server.listen(port, () => {
   console.log(`http://localhost:${port}`);
   console.log('메이플 교차 선택기 실행 됨!');
 });
