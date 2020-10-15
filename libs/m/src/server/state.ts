c7549d9ef61e109802fee68c98b068e0c1df8f37
Pdom <ahb0327@naver.com>
2020-10-15 20:40:20 +0900

    Create: state

--- libs/m/src/server/state.ts
+++ libs/m/src/server/state.ts
@@ -1,12 +1,10 @@
 import { Event, JobId } from '../common/enums';
 import { JobList } from '../common/jobs';
+import SequenceQueue, { Sequence } from './sequenceQueue';
 
 export default class State {
-  private _teamBanSize: number;
-  private _teamPickSize: number;
-  private _banIndex = 1;
-  private _pickIndex = 1;
-  private _nextEvent: Event = Event.START;
+  sequenceQueue: SequenceQueue;
+  nextSequence?: Sequence;
 
   unPickedList: JobId[];
   leftBanList: JobId[] = [];
@@ -14,10 +12,9 @@ export default class State {
   leftPickList: JobId[] = [];
   rightPickList: JobId[] = [];
 
-  constructor(teamBanSize: number, teamPickSize: number) {
-    this._teamBanSize = teamBanSize;
-    this._teamPickSize = teamPickSize;
-
+  constructor() {
+    this.sequenceQueue = new SequenceQueue();
+    this.nextSequence = this.sequenceQueue.dequeue();
     this.unPickedList = JobList.map<JobId>((value) => {
       return value.id;
     });
@@ -31,23 +28,6 @@ export default class State {
       return true;
     } else {
       return false;
-    }
-  }
-
-  onBan(jobId: JobId) {
-    if (this._banIndex < this._teamBanSize * 2) {
-      throw new Error('이미 밴이 완료되었습니다!');
-    }
-    if (this.removeUnPickedJob(jobId)) {
-      if (this._banIndex % 2) {
-        this.leftBanList.push(jobId);
-      } else {
-        this.rightBanList.push(jobId);
-      }
-      this._banIndex++;
-      return;
-    } else {
-      throw new Error('선택 할 수 없는 직업입니다!');
     }
   }
 }
