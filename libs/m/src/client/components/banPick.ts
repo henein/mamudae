67616af0ae50f5f622bf1f35a4220670eec40c80
Pdom <ahb0327@naver.com>
2020-10-23 00:33:41 +0900

    Update: 밴픽창 토글 버튼

--- libs/m/src/client/components/banPick.ts
+++ libs/m/src/client/components/banPick.ts
@@ -1,5 +1,5 @@
 import { Scrollbox } from 'pixi-scrollbox';
-import TWEEN from '@tweenjs/tween.js';
+import TWEEN, { Tween } from '@tweenjs/tween.js';
 import { Job, jobList } from '../../common/jobs';
 import { constants } from '../constants';
 
@@ -21,50 +21,77 @@ function makePortraitButton(job: Job) {
   return container;
 }
 
+let isVision = false;
+
 export function createBanPickModal() {
   const container = new PIXI.Container();
+  const modal = container.addChild(new PIXI.Container());
 
-  const graphics = new PIXI.Graphics();
+  const graphics = modal.addChild(new PIXI.Graphics());
   graphics.beginFill(0xffffff, 0.9);
   graphics.drawRoundedRect(0, 0, 928, 672, 24);
   graphics.endFill();
-  container.addChild(graphics);
 
-  const scrollbox = new Scrollbox({
-    boxWidth: 928,
-    boxHeight: 576,
-    fade: true,
-    fadeScrollboxWait: 1000,
-    fadeScrollbarTime: 300,
-    scrollbarForegroundAlpha: 0.8,
-    scrollbarBackgroundAlpha: 0,
-    scrollbarOffsetVertical: -16,
-    scrollbarSize: 4,
-  });
+  const scrollbox = modal.addChild(
+    new Scrollbox({
+      boxWidth: 928,
+      boxHeight: 576,
+      fade: true,
+      fadeScrollboxWait: 1000,
+      fadeScrollbarTime: 300,
+      scrollbarForegroundAlpha: 0.8,
+      scrollbarBackgroundAlpha: 0,
+      scrollbarOffsetVertical: -16,
+      scrollbarSize: 4,
+    })
+  );
   scrollbox.position.set(0, 48);
-  container.addChild(scrollbox);
 
-  const widthBox = new PIXI.Graphics();
+  const widthBox = scrollbox.content.addChild(new PIXI.Graphics());
   widthBox.beginFill(0xffffff, 0);
   widthBox.drawRect(0, 0, 928, 10);
   widthBox.endFill();
-  scrollbox.content.addChild(widthBox);
 
   for (let i = 0; i < jobList.length; i++) {
-    //const portraitButton = makePortraitButton(i + 1);
-    const portraitButton = makePortraitButton(jobList[i]);
+    const portraitButton = scrollbox.content.addChild(
+      makePortraitButton(jobList[i])
+    );
+
     portraitButton.position.set(48 + 176 * (i % 5), 168 * Math.floor(i / 5));
-    scrollbox.content.addChild(portraitButton);
   }
   scrollbox.update();
 
-  container.position.set(constants.BASE_WIDTH / 2, 648 + 24);
-  container.alpha = 0;
-  container.pivot.set(container.width / 2, container.height / 2);
+  modal.position.set(constants.BASE_WIDTH / 2, 648 + 24);
+  modal.alpha = 0;
+  modal.pivot.set(modal.width / 2, modal.height / 2);
 
-  new TWEEN.Tween(container)
-    .to({ position: { y: '-24' }, alpha: 1 }, 200)
-    .start();
+  const appearTween = new TWEEN.Tween(modal).to(
+    { position: { y: 648 }, alpha: 1 },
+    200
+  );
 
+  const disappearTween = new TWEEN.Tween(modal).to(
+    { position: { y: 648 + 24 }, alpha: 0 },
+    200
+  );
+
+  const toggleButton = container.addChild(new PIXI.Graphics());
+  toggleButton.beginFill(0xffffff, 1);
+  toggleButton.drawRect(1920 / 2 - 50, 1080 - 60 - 16, 100, 48);
+  toggleButton.endFill();
+  toggleButton.interactive = true;
+  toggleButton.buttonMode = true;
+  toggleButton.on('pointerdown', () => {
+    console.log(isVision);
+    if (isVision) {
+      isVision = false;
+      appearTween.stop();
+      disappearTween.start();
+    } else {
+      isVision = true;
+      disappearTween.stop();
+      appearTween.start();
+    }
+  });
   return container;
 }
