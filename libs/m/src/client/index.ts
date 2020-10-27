0e4227bfd4bc7d7d75a8c039cc2a28eff7eced6f
Pdom <ahb0327@naver.com>
2020-10-28 01:02:42 +0900

    Update: 배달의민족 주아 폰트 적용

--- libs/m/src/client/index.ts
+++ libs/m/src/client/index.ts
@@ -14,77 +14,105 @@ const app = new PIXI.Application({
   width: 1920,
   height: 1080,
 });
-app.renderer.autoDensity = true;
+document.body.appendChild(app.view);
 
-function createBlurOverlay() {
-  const container = new PIXI.Container();
-
-  const blurFilter = new PIXI.filters.BlurFilter();
-  blurFilter.blur = 16;
-
-  const graphics = PIXI.Sprite.from('./assets/backgrounds/multiply.png');
-  graphics.filters = [new PIXI.picture.MaskFilter(blurFilter)];
-  container.addChild(graphics);
-
-  const multiply = PIXI.Sprite.from('./assets/backgrounds/multiply.png');
-  multiply.blendMode = PIXI.BLEND_MODES.MULTIPLY;
-  container.addChild(multiply);
-
-  return container;
+declare global {
+  interface Window {
+    WebFontConfig: any;
+  }
 }
 
-const baseContainer = new PIXI.Container();
+window.WebFontConfig = {
+  google: {
+    families: ['Jua'],
+  },
 
-baseContainer.addChild(PIXI.Sprite.from('./assets/backgrounds/10.png'));
+  active() {
+    onAssetsLoaded();
+  },
+};
 
-baseContainer.addChild(PIXI.Sprite.from('./assets/splashes/10.png'));
+/* eslint-disable */
+(function() {
+  const wf = document.createElement('script');
+  wf.src = `${document.location.protocol === 'https:' ? 'https' : 'http'
+  }://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js`;
+  wf.type = 'text/javascript';
+  wf.async = true;
+  const s = document.getElementsByTagName('script')[0];
+  s.parentNode?.insertBefore(wf, s);
+}());
+/* eslint-enabled */
 
-baseContainer.addChild(createBlurOverlay());
+function onAssetsLoaded() {
+  function createBlurOverlay() {
+    const container = new PIXI.Container();
 
-baseContainer.addChild(PIXI.Sprite.from('./assets/backgrounds/cameraUI.png'));
+    const blurFilter = new PIXI.filters.BlurFilter();
+    blurFilter.blur = 16;
 
-baseContainer.addChild(new TitleBar());
+    const graphics = PIXI.Sprite.from('./assets/backgrounds/multiply.png');
+    graphics.filters = [new PIXI.picture.MaskFilter(blurFilter)];
+    container.addChild(graphics);
 
-baseContainer.addChild(new BanViewer());
+    const multiply = PIXI.Sprite.from('./assets/backgrounds/multiply.png');
+    multiply.blendMode = PIXI.BLEND_MODES.MULTIPLY;
+    container.addChild(multiply);
 
-baseContainer.addChild(new PickViewer());
+    return container;
+  }
 
-baseContainer.addChild(new BanPickModal());
+  const baseContainer = new PIXI.Container();
 
-const text = baseContainer.addChild(
-  new PIXI.Text('시퀸스', { fontSize: 50, fill: '#ffffff' })
-);
-text.position.set(500, 500);
-autorun(() => {
-  text.text = store.sequenceStore.nextSequence?.event.toString() ?? '';
-});
+  baseContainer.addChild(PIXI.Sprite.from('./assets/backgrounds/10.png'));
 
-baseContainer.pivot.set(constants.BASE_WIDTH / 2, constants.BASE_HEIGHT / 2);
-app.stage.addChild(baseContainer);
+  baseContainer.addChild(PIXI.Sprite.from('./assets/splashes/10.png'));
 
-app.stage.filters = [new PIXI.filters.AlphaFilter()];
-app.stage.filterArea = app.screen;
+  baseContainer.addChild(createBlurOverlay());
+
+  baseContainer.addChild(PIXI.Sprite.from('./assets/backgrounds/cameraUI.png'));
+
+  baseContainer.addChild(new TitleBar());
+
+  baseContainer.addChild(new BanViewer());
+
+  baseContainer.addChild(new PickViewer());
+
+  baseContainer.addChild(new BanPickModal());
+
+  const text = baseContainer.addChild(
+    new PIXI.Text('시퀸스', { fontFamily: 'Jua', fontSize: 50, fill: '#ffffff' })
+  );
+  text.position.set(500, 500);
+  autorun(() => {
+    text.text = store.sequenceStore.nextSequence?.event.toString() ?? '';
+  });
+
+  baseContainer.pivot.set(constants.BASE_WIDTH / 2, constants.BASE_HEIGHT / 2);
+  app.stage.addChild(baseContainer);
+
+  app.stage.filters = [new PIXI.filters.AlphaFilter()];
+  app.stage.filterArea = app.screen;
+
+  window.addEventListener('resize', onResize);
+
+  function onResize() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const ratio = width / height;
+
+    app.renderer.resize(width, height);
+
+    if (ratio < constants.BASE_RATIO) {
+      baseContainer.scale.set(width / constants.BASE_WIDTH);
+    } else {
+      baseContainer.scale.set(height / constants.BASE_HEIGHT);
+    }
+    baseContainer.position.set(width / 2, height / 2);
+  }
+  onResize();
+}
 
 app.ticker.add(() => {
   TWEEN.update();
 });
-
-document.body.appendChild(app.view);
-
-window.addEventListener('resize', onResize);
-
-function onResize() {
-  const width = window.innerWidth;
-  const height = window.innerHeight;
-  const ratio = width / height;
-
-  app.renderer.resize(width, height);
-
-  if (ratio < constants.BASE_RATIO) {
-    baseContainer.scale.set(width / constants.BASE_WIDTH);
-  } else {
-    baseContainer.scale.set(height / constants.BASE_HEIGHT);
-  }
-  baseContainer.position.set(width / 2, height / 2);
-}
-onResize();
