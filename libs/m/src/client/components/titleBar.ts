1cc487b66fcf049fa5a8ad1f3e6203f0ad6f047a
Pdom <ahb0327@naver.com>
2020-10-30 01:50:50 +0900

    Update: 타이틀바 애니메이션

--- libs/m/src/client/components/titleBar.ts
+++ libs/m/src/client/components/titleBar.ts
@@ -1,14 +1,38 @@
 import { autorun } from 'mobx';
+import { GlowFilter } from 'pixi-filters';
 import { IOEvent } from '../../common/events';
 import { store } from '../store';
 import { DetailRoundedRect } from './detailRoundedRect';
-import { MultiStyleText } from './text';
+import { Tween, Easing } from '@tweenjs/tween.js';
+import MultiStyleText from 'pixi-multistyle-text';
 
 export class TitleBar extends PIXI.Container {
   private _main: PIXI.Graphics;
+  private _leftGlow: PIXI.Sprite;
+  private _rightGlow: PIXI.Sprite;
+  private _leftArrow: PIXI.Sprite;
+  private _rightArrow: PIXI.Sprite;
+  private _leftTween: Tween<any>;
+  private _rightTween: Tween<any>;
+  private _preLeftTween: Tween<any>;
+  private _preRightTween: Tween<any>;
 
   constructor() {
     super();
+
+    this._leftGlow = this.addChild(
+      PIXI.Sprite.from('../assets/ui/leftGlow.png')
+    );
+    this._leftGlow.position.set(0, 0);
+    this._leftGlow.visible = false;
+    this._leftGlow.alpha = 0;
+
+    this._rightGlow = this.addChild(
+      PIXI.Sprite.from('../assets/ui/rightGlow.png')
+    );
+    this._rightGlow.position.set(1360, 0);
+    this._rightGlow.visible = false;
+    this._rightGlow.alpha = 0;
 
     this._main = this.addChild(
       new DetailRoundedRect({
@@ -21,6 +45,81 @@ export class TitleBar extends PIXI.Container {
         bottomRight: 64,
       })
     );
+
+    this._leftArrow = this.addChild(
+      PIXI.Sprite.from('../assets/ui/leftArrow.png')
+    );
+    this._leftArrow.position.set(428, 0);
+    this._leftArrow.visible = false;
+    this._leftArrow.alpha = 0;
+
+    this._preLeftTween = new Tween({
+      glow: this._leftGlow,
+      arrow: this._leftArrow,
+    })
+      .to({ glow: { alpha: 1 }, arrow: { alpha: 0.7 } }, 500)
+      .easing(Easing.Quartic.InOut)
+      .onStart((object) => {
+        object.glow.visible = true;
+        object.glow.alpha = 0;
+        object.arrow.visible = true;
+        object.arrow.alpha = 0;
+      });
+
+    this._leftTween = new Tween({
+      glow: this._leftGlow,
+      arrow: this._leftArrow,
+    })
+      .to({ glow: { alpha: 0.7 }, arrow: { alpha: 1 } }, 1000)
+      .repeat(Infinity)
+      .yoyo(true)
+      .onStop((object) => {
+        object.glow.visible = false;
+        object.glow.alpha = 0;
+        object.arrow.visible = false;
+        object.arrow.alpha = 0;
+      });
+
+    this._rightArrow = this.addChild(
+      PIXI.Sprite.from('../assets/ui/rightArrow.png')
+    );
+    this._rightArrow.position.set(1356, 0);
+    this._rightArrow.visible = false;
+    this._rightArrow.alpha = 0;
+
+    this._preRightTween = this._rightTween = new Tween({
+      glow: this._rightGlow,
+      arrow: this._rightArrow,
+    })
+      .to({ glow: { alpha: 1 }, arrow: { alpha: 0.7 } }, 500)
+      .easing(Easing.Quartic.InOut)
+      .onStart((object) => {
+        object.glow.visible = true;
+        object.glow.alpha = 0;
+        object.arrow.visible = true;
+        object.arrow.alpha = 0;
+      });
+
+    this._rightTween = new Tween({
+      glow: this._rightGlow,
+      arrow: this._rightArrow,
+    })
+      .to({ glow: { alpha: 0.7 }, arrow: { alpha: 1 } }, 1000)
+      .repeat(Infinity)
+      .yoyo(true)
+      .onStop((object) => {
+        object.glow.visible = false;
+        object.glow.alpha = 0;
+        object.arrow.visible = false;
+        object.arrow.alpha = 0;
+      });
+
+    const glowFilter = new GlowFilter({
+      distance: 12,
+      quality: 0.3,
+      outerStrength: 1,
+    });
+    this._main.filters = [glowFilter];
 
     const title = this.addChild(
       new MultiStyleText('', {
@@ -46,43 +145,54 @@ export class TitleBar extends PIXI.Container {
 
     autorun(() => {
       const nextSequence = store.sequenceStore.nextSequence;
+      this._preLeftTween.stop();
+      this._leftTween.stop();
+      this._preRightTween.stop();
+      this._rightTween.stop();
 
       if (nextSequence) {
         switch (nextSequence.event) {
           case IOEvent.START:
-            title.setText('대기 중');
+            title.text = '대기 중';
             break;
           case IOEvent.BAN_PICK:
             if (nextSequence.payload?.action == 'ban') {
-              title.setText(
-                `${
-                  nextSequence.payload.team == 'left'
-                    ? '<leftTeam>나초팀</leftTeam>'
-                    : '<rightTeam>금앙팀</rightTeam>'
-                }이 ${
-                  (nextSequence.payload.index ?? 0) + 1
-                }번째 밴할 직업을 선택 중`
-              );
+              title.text = `${
+                nextSequence.payload.team == 'left'
+                  ? '<leftTeam>나초팀</leftTeam>'
+                  : '<rightTeam>금앙팀</rightTeam>'
+              }이 ${
+                (nextSequence.payload.index ?? 0) + 1
+              }번째 밴할 직업을 선택 중`;
             } else if (nextSequence.payload?.action == 'pick') {
-              title.setText(
-                `${
-                  nextSequence.payload?.team == 'left'
-                    ? '<leftTeam>나초팀</leftTeam>'
-                    : '<rightTeam>금앙팀</rightTeam>'
-                }이 ${
-                  (nextSequence.payload?.index ?? 0) + 1
-                }번째 직업을 선택 중`
-              );
+              title.text = `${
+                nextSequence.payload?.team == 'left'
+                  ? '<leftTeam>나초팀</leftTeam>'
+                  : '<rightTeam>금앙팀</rightTeam>'
+              }이 ${(nextSequence.payload?.index ?? 0) + 1}번째 직업을 선택 중`;
             }
+
+            switch (nextSequence?.payload?.team) {
+              case 'left':
+                this._preLeftTween.chain(this._leftTween).start();
+                break;
+              case 'right':
+                this._preRightTween.chain(this._rightTween).start();
+                break;
+              default:
+                this._preLeftTween.chain(this._leftTween).start();
+                this._preRightTween.chain(this._rightTween).start();
+            }
+
             break;
           case IOEvent.END:
-            title.setText('종료');
+            title.text = '종료';
             break;
           default:
-            title.setText('');
+            title.text = '';
         }
       } else {
-        title.setText('');
+        title.text = '';
       }
     });
   }
