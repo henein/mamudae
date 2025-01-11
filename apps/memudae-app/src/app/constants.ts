fe8742c771ddb88967e5acb159860e44e57eb573
Pdom <pdom0327@gmail.com>
2025-01-11 11:40:26 +0900

    feat(mamudae): init

---
+++ apps/memudae-app/src/app/constants.ts
@@ -0,0 +1,4 @@
+export const rendererAppPort = 4200;
+export const rendererAppName = 'mamudae'; // options.name.split('-')[0] + '-web'
+export const electronAppName = 'mamudae-app';
+export const updateServerUrl = 'https://deployment-server-url.com'; // TODO: insert your update server url here
