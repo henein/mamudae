8df5b128887ce544286f069515a914c0698a74d6
Pdom <pdom0327@gmail.com>
2025-02-28 20:23:19 +0900

    feat: 찐찐막

--- libs/maple-pick/src/app/screens/main/components/banViewer.ts
+++ libs/maple-pick/src/app/screens/main/components/banViewer.ts
@@ -1,7 +1,7 @@
 import { store } from '../../../store/state-store';
 import { BanPanel } from './banPanel';
 import { autorun } from 'mobx';
-import { Container, Graphics } from 'pixi.js';
+import { Container, Graphics, Text, TextStyle } from 'pixi.js';
 
 export class BanViewer extends Container {
   private _nextPanel?: BanPanel;
@@ -31,19 +31,74 @@ export class BanViewer extends Container {
     voteBg.endFill();
     voteBg.position.set(760, 830);
 
+    const text = voteBg.addChild(
+      new Text(
+        'VOTE',
+        new TextStyle({
+          fontFamily: 'Maplestory Bold',
+          fontSize: 24,
+          fill: '#ffffffff',
+          align: 'center',
+        }),
+      ),
+    );
+    text.anchor.set(0.5, 0.5);
+    text.position.set(158 + 42, 83 - 20);
+
     const ban1 = voteBg.addChild(new BanPanel(this._size));
     ban1.portrait.jobId = store.roomState.votedBan;
-    ban1.position.set(158 - 84 - 32, 83);
+    ban1.position.set(158 - 84 - 32, 83 + 12);
 
-    // const text1
+    const text1 = voteBg.addChild(
+      new Text(
+        'BAN',
+        new TextStyle({
+          fontFamily: 'Maplestory Light',
+          fontSize: 20,
+          fill: '#ffffffff',
+          align: 'center',
+        }),
+      ),
+    );
+    text1.anchor.set(0.5, 0.5);
+    text1.position.set(158 - 84 - 32 + 42, 83 + 84 + 20 + 12);
 
     const ban2 = voteBg.addChild(new BanPanel(this._size));
     ban2.portrait.jobId = store.roomState.votedPicks[0];
-    ban2.position.set(158, 83);
+    ban2.position.set(158, 83 + 12);
+
+    const text2 = voteBg.addChild(
+      new Text(
+        '1st',
+        new TextStyle({
+          fontFamily: 'Maplestory Light',
+          fontSize: 20,
+          fill: '#ffffffff',
+          align: 'center',
+        }),
+      ),
+    );
+    text2.anchor.set(0.5, 0.5);
+    text2.position.set(158 + 42, 83 + 84 + 20 + 12);
+
 
     const ban3 = voteBg.addChild(new BanPanel(this._size));
     ban3.portrait.jobId = store.roomState.votedPicks[1];
-    ban3.position.set(158 + 84 + 32, 83);
+    ban3.position.set(158 + 84 + 32, 83 + 12);
+
+    const text3 = voteBg.addChild(
+      new Text(
+        '2nd',
+        new TextStyle({
+          fontFamily: 'Maplestory Light',
+          fontSize: 20,
+          fill: '#ffffffff',
+          align: 'center',
+        }),
+      ),
+    );
+    text3.anchor.set(0.5, 0.5);
+    text3.position.set(158 + 84 + 32 + 42, 83 + 84 + 20 + 12);
 
     autorun(() => {
       // eslint-disable-next-line @typescript-eslint/no-unused-expressions
